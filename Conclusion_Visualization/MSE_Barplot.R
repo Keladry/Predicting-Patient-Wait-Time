071c393c904fbaff905191a6084e9bca27ccbbdc
Marika Fox <marikakyranna@gmail.com>
2021-01-27 18:26:02 -0500

    Update MSE_Barplot.R

--- Conclusion_Visualization/MSE_Barplot.R
+++ Conclusion_Visualization/MSE_Barplot.R
@@ -1,30 +1,32 @@
 #plots the MSE values for the models we've tried, for easy comparison of effectiveness
 
 #list MSEs calculated from each model
-MLR_MSE <- 706.915
+MLR_MSE <- 705.17
 
-RF_MSE_1 <- 662.722
-RF_MSE_2 <- 5 #placeholder
-  
-SVR_MSE_Radial <- 668.398
-SVR_MSE_Linear <- 729.173
-SVR_MSE_Polynomial <- 710.652
+RF_MSE <- 662.722
+
+SVR_MSE_R <- 668.398
+SVR_MSE_L <- 729.173
+SVR_MSE_P <- 710.652
 
 #makes vector of values
-MSE_list <- c(MLR_MSE, RF_MSE_1, RF_MSE_2, SVR_MSE_Radial, SVR_MSE_Linear,
-              SVR_MSE_Polynomial)
+MSE_list <- c(MLR_MSE, SVR_MSE_L, SVR_MSE_P, SVR_MSE_R, RF_MSE)
 #how much space will be before each bar
-barspacing <- c(0, 1, .1, 1, .1, .1)
+barspacing <- c(.5,1.5,.1,.1,1.5)
 #labels for bars
-barnames <- c("Multiple Linear Regression","Random Forest\n Before Tuning",
-              "Random Forest\n After Tuning","SVR\n Radial Kernel",
-              "SVR\n Linear Kernel","SVR\n Polynomial Kernel")
+barnames <- c("Linear Regression","SVR\n Linear Kernel","SVR\n Polynomial Kernel",
+              "SVR\n Radial Kernel","Random Forest")
 #sets colours
-colours <- c("#00FF00","#99CCFF","#0033FF","#CC99FF","#9900FF","#663399")
+green <- "#99FF99"
+blue <- "#99CCFF"
+purple <- "#CC99FF"
+colours <- c(greens, blues, blues, blues, purples)
 
 #creates barplot
 barplot <- barplot(MSE_list, space=barspacing, names.arg=barnames, col=colours,
-        main="Mean Squared Error\n For Various Models", ylab="MSE",
-        ylim=c(0,750), axes=TRUE, yaxp = c(0, 750, 15))
+                   main="Mean Squared Error For Various Model Types\n The random forest model had the smallest MSE and thus the best performance",
+                   ylab="MSE",
+                   font.lab=2,ylim=c(0,750), axes=TRUE, yaxp = c(0, 750, 15))
 #adds text with exact values for each MSE on the corresponding bar
-text(barplot, 30, round(MSE_list, 1),cex=1.1,pos=3, font=2) 
+text(barplot, 30, round(MSE_list, 0),cex=1.1,pos=3, font=2)
+
